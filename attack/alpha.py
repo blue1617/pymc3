76732b708ed68a5a66b0920bae546b431110f895
Alina <alina_preda200@yahoo.com>
2019-03-31 18:56:40 +0300

    adding alpha programme

--- attack/alpha.py
+++ attack/alpha.py
@@ -1,29 +1,13 @@
+from functools import reduce
 
-def alpha (records, reducer):
-    d = {}
-    for record in records:
-        if record is not None:
-            key, value = record
-            if key in d:
-                d[key].append(value)
-            else:
-                d[key] = [value]
-    return d
+def alpha (nameAgeList):
+    ageList = list(map(lambda args: (args[1],1), nameAgeList))
+    addedAgeList = list(reduce(lambda x, y: (x[0] + y[0], x[1]+y[1]), ageList))
+    averageAge=reduce(lambda sum, count: sum/count, addedAgeList)
+    return averageAge
 
-#records
-#.map
-#{ case (n,a) => (a,1) }
-#.reduce { case (x,y) => (x._1+y._1, x._2 + y._2) }
-#.map
-#{ case (sum,count) => sum / count }
-ageList = [("Alice", 10),("Alice", 15),("Bob", 30)]
-listResult= alpha(ageList, lambda x, y: x + y/2)
-print("list result is ", listResult )
-average = 0
-for record in listResult:
-    ageList = record[1]
-    averagePerName=0
-    for age in ageList:
-        averagePerName+=averagePerName + age /2
-    average += average + averagePerName
-print("average age is", average)
+nameAgeList = [("Alice", 10),("Alice", 15),("Bob", 30)]
+averageAge=alpha(nameAgeList)
+print (averageAge)
+
+
